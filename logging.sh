4242cd6012cec57a8581fe385768df4ba15129b4
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2017-09-25 15:08:20 +0200

    Fix formatting issues
    * Fixes some formatting issues when using IFS to join args. The
      formatter should now receive both piped and non-piped args as separate
      arguments.
    * Fixes an issue where the formatter could only receive one message argument.
    * Fixes level lookup, so that custom methods for level lookup can be
      implemented.

--- logging.sh
+++ logging.sh
@@ -10,9 +10,33 @@ LOG_LEVELS=([DEBUG]=7 [INFO]=6 [NOTICE]=5 [WARNING]=4 [ERROR]=3 [CRITICAL]=2 [AL
 # Current (default) filter level
 LOG_LEVEL=${LOG_LEVELS[WARNING]}
 
+# get_level_value <DEBUG | INFO | ...>
+#
+#   Get the level value from level name
+#
+get_level_value() {
+    local level="${1}" value=""
+    [ -z "${LOG_LEVELS[$level]+isset}" ] && return 1
+    echo "${LOG_LEVELS[$level]}"
+}
+
+# get_level_name <0..7>
+#
+#   Get log level name from numeric value.
+#
+get_level_name() {
+    local level="" value="${1}"
+    for level in "${!LOG_LEVELS[@]}"; do
+       [ "${LOG_LEVELS[$level]}" -eq "${value}" ] && echo "${level}" && return 0
+    done
+    return 1
+}
+
+
+
 # log <level> <msg> [msg...]
 #
-#   Format log line and write to stderr. 
+#   Format log line and write to stderr.
 #
 log() {
     _logging_log "${@}";
@@ -21,15 +45,18 @@ log() {
 # _logging_fmt <level> <msg> [msg...]
 #
 #   Print formatted message.  Redefine for custom formatting.
+#
 #   This implementation formats lines as:
 #     YYYY-mm-dd HH:MM:SS+ZZ:ZZ LEVEL (func_name) Message
+#
+#   Message args are concatenated with $IFS
 #
 _logging_fmt() {
     # caller is three stackframes back (_logging_fmt, _logging_log, log, <caller>)
     local level="$1" cmd="${FUNCNAME[3]}"
     shift
     printf "%s %s (%s) %s\n" "$(date +%Y-%m-%dT%H:%M:%S%z)" \
-            "$level" "$cmd" "$@"
+            "$level" "$cmd" "$*"
 }
 
 # _logging_out <msg> [msg...]
@@ -46,32 +73,25 @@ _logging_out() {
 #   formats and outputs a log record.
 #
 _logging_log() {
-    local level="$1" line=""
+    local level="$1" lvl="" line=""
+    local -a args
     shift
-    [ -z "${LOG_LEVELS[$level]+isset}" ] && return 1
-    [ "${LOG_LEVELS[$level]}" -gt "$LOG_LEVEL" ] && return 0
+    lvl="$(get_level_value "$level")"
+    [ -z "$lvl" ] && return 1
+    [ "${lvl}" -gt "$LOG_LEVEL" ] && return 0
     if [ -t 0 ]; then
         _logging_out "$(_logging_fmt "$level" "$@")"
     else
         while read -r line; do
+            # TODO: Reset IFS here so that $@ is split correctly?
+            args=()
             if [ -n "$*" ]; then
-                line="$* $line";
+                args+=( "$@" )
             fi
-            _logging_out "$(_logging_fmt "$level" "$line")"
+            args+=( "${line}" );
+            _logging_out "$(_logging_fmt "$level" "${args[@]}")"
         done
     fi
-}
-
-# get_level_name <0..7>
-#
-#   Get log level name from numeric value.
-#
-get_level_name() {
-    local level="" value="${1}"
-    for level in "${!LOG_LEVELS[@]}"; do
-       [ "${LOG_LEVELS[$level]}" -eq "${value}" ] && echo "${level}" && return 0
-    done
-    return 1
 }
 
 # helper functions for each level
